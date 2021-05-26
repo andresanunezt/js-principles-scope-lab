f98a88a882eed10d38f22478d7cbfa999bb4333a
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-26 16:45:46 -0400

    commit tree fa30f864818303251bd4ff714ed22a967084bb66

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+const leastFavoriteCustomer = 'sucky customer';
+
+
+function upperCaseCustomerName() {
+
+    customerName = 'BOB';
+}
+
+
+function setBestCustomer() {
+   
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    
+    bestCustomer = 'maybe bob';
+}
+
+
+
+
+function changeLeastFavoriteCustomer() {
+
+    leastFavoriteCustomer = 'sucky customer 2';
+
+}
